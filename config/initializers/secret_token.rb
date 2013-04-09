dea8cba16cec5851f33f3b73859b34a5b9aeb5ae
ghlingjun <zh.lingjun@gmail.com>
2013-04-09 16:23:33 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YlOa::Application.config.secret_token = '5cbd0e0eecebba6a804d0ebfd3f6d415fb0bc30196bc2ff4e4cc704fa9a1d09c46cd58875cc4aac851676db43625aba2669e6d22f59d8990db090e85b2bf0112'

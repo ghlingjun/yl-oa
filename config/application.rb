07294054d82ee6cb4e687cdc78f903bccd9d3573
Ethan <zh.lingjun@gmail.com>
2013-09-29 15:42:31 +0800

    change yunlingOA to OA

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module YunlingOA
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.before_configuration do
       I18n.locale = "zh-CN".to_sym
-      I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+      I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb, yml}')]
       I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb, yml}')]
       I18n.reload!
     end

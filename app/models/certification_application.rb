67253810adaba8db015c88bd70dcc48ada066487
ghlingjun <zh.lingjun@gmail.com>
2013-11-18 13:47:47 +0800

    close the work log function

--- app/models/certification_application.rb
+++ app/models/certification_application.rb
@@ -1,4 +1,18 @@
 class CertificationApplication < ActiveRecord::Base
   attr_accessible :back_date, :certificater_name, :details, :out_date, :real_user_name, :reason, :use_mode, :user_id
   belongs_to :user
+
+  validates :reason, :presence => true
+
+  USE = [
+    I18n.t("init_data.certification_application.use.official_business"),
+    I18n.t("init_data.certification_application.use.persinal_business"),
+    I18n.t("init_data.application.use.other")
+  ]
+
+  USE_MODE = [
+  	I18n.t("init_data.certification_application.use_mode.stamp"),
+    I18n.t("init_data.certification_application.use_mode.copy"),
+    I18n.t("init_data.certification_application.use_mode.borrow")
+  ]
 end
